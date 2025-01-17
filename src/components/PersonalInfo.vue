39a02d7b3ce0f0f0ec9203ec323462e962dad267
Ikenna Richard <oguejioforrichard@gmail.com>
2025-01-17 16:42:39 +0100

    feat: add personal info

---
+++ src/components/PersonalInfo.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import FormInput from './FormInput.vue';
+
+const model = reactive({
+  username: '',
+  email: '',
+  phoneNumber: ''
+})
+</script>
+
+<template>
+  <h2>Personal Info</h2>
+  <p>Please provide your name, email address, and phone number.</p>
+
+  <div>
+    <FormInput
+      v-model="model.username"
+      label="Name"
+      id="username"
+      name="username"
+      placeholder="e.g. Stephen King"
+    />
+    <FormInput
+      v-model="model.email"
+      label="Email Address"
+      id="email"
+      name="email"
+      type="email"
+      placeholder="e.g. stephenking@lorem.com"
+    />
+    <FormInput
+      v-model="model.phoneNumber"
+      label="Phone Number"
+      id="phone"
+      name="phone"
+      placeholder="e.g. +1234 567 890"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
