74c5fa58e00aae1b840aaea91ae9a8f413ab6c6c
Ikenna Richard <oguejioforrichard@gmail.com>
2025-01-17 16:42:17 +0100

    chore: update form components

---
+++ src/components/FormButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const {type = 'button'} = defineProps<{
+    label: string,
+    type?: "button" | "submit" | "reset" | undefined,
+  }>()
+  defineEmits(['handleClick'])
+// const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <button @click="$emit('handleClick')" :type="type">{{label}}</button>
+</template>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  const {type='text'} = defineProps<{
+    label: string,
+    placeholder: string,
+    id: string,
+    type?: string,
+    name: string
+  }>()
+
+  const model = defineModel()
+</script>
+
+<template>
+  <div>
+    <label :for="id">{{label}}</label>
+    <input :type="type" :placeholder="placeholder" v-model="model" :id="id" :name="name" required>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
