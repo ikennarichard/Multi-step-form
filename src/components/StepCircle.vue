8d19fce46e0babc042dd825f1b6462008c80f6b6
Ikenna Richard <oguejioforrichard@gmail.com>
2025-01-17 16:43:00 +0100

    refactor code

--- src/components/StepCircle.vue
+++ src/components/StepCircle.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+
 defineProps<{
-  msg: string
+  step: number;
 }>()
 
-const status = ref(true)
-const home = 'essentials.com'
-const newTask = ref('')
-const tasks = ref(['First, task'])
 
-const toggleStatus = () => {
-  if (status.value) {
-    status.value = false
-  }
-}
-
-const addTask = () => {
-  if (newTask.value.trim() != '') {
-    tasks.value.push(newTask.value)
-  }
-  return
-}
 </script>
 
 <template>
@@ -30,9 +15,7 @@ const addTask = () => {
 
   <!-- form -->
   <form @submit.prevent="addTask">
-    <label for="new-task">New Task</label>
-    <input type="text" v-model="newTask" id="new-task" />
-    <button>Add task</button>
+   
   </form>
 
   <ul>
@@ -41,28 +24,3 @@ const addTask = () => {
     </li>
   </ul>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
